df4da619155470b9c0a09cc60cc11de2bff2ae48
EC2 Default User <ec2-user@ip-172-31-16-124.ap-northeast-1.compute.internal>
2023-11-29 09:20:13 +0000

    [Finish]いいね機能

---
+++ app/models/favorite.rb
@@ -0,0 +1,6 @@
+class Favorite < ApplicationRecord
+  belongs_to :book
+  belongs_to :user
+  
+  validates :user_id, uniqueness: { scope: :book_id }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get "home/about" => "homes#about", as: "about"
 
   resources :users, only: [:show, :index, :edit, :update]
-  resources :books, only: [:show, :create, :index, :edit, :update, :destroy]
+  resources :books, only: [:show, :create, :index, :edit, :update, :destroy] do
+    resource :favorite, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
